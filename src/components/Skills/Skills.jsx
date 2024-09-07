c9dc0e7ec2fa43391ac6e6ebbca1d1e570d2610e
YasinzHyper <zulfiyasser@gmail.com>
2024-09-07 20:29:26 +0530

    images path

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -8,46 +8,46 @@ function Skills() {
         </div>
         <div className="skill-grid">
           <div className="skill-card">
-            <img src="./images/android-logo.png" alt="img" />
+            <img src="/android-logo.png" alt="img" />
             <p> Android Development</p>
           </div>
           <div className="skill-card">
-            <img src="./images/flutter-logo.png" alt="img" />
+            <img src="/flutter-logo.png" alt="img" />
             <p>Flutter/Dart</p>
           </div>
           <div className="skill-card">
-            <img src="./images/JavaScript-logo.png" alt="img" />
+            <img src="/JavaScript-logo.png" alt="img" />
             <p>JavaScript</p>
           </div>
           <div className="skill-card">
-            <img src="./images/react-logo.png" alt="img" />
+            <img src="/react-logo.png" alt="img" />
             <p> React.js</p>
           </div>
           <div className="skill-card">
-            <img src="./images/firebase-logo.png    " alt="img" />
+            <img src="/firebase-logo.png    " alt="img" />
             <p> FireBase </p>
           </div>
           <div className="skill-card">
-            <img src="./images/css-logo.png" alt="img" />
+            <img src="/css-logo.png" alt="img" />
             <p> HTML/CSS</p>
           </div>
           <div className="skill-card">
-            <img src="./images/nodejs-logo.png" alt="img" />
+            <img src="/nodejs-logo.png" alt="img" />
             <p> Node.js </p>
           </div>
           <div className="skill-card">
-            <img src="./images/postgresql-logo.png" alt="img" />
+            <img src="/postgresql-logo.png" alt="img" />
             <p>PostgreSQL</p>
           </div>
           <div className="skill-card">
-            <img src="./images/express-logo.png" alt="img" />
+            <img src="/express-logo.png" alt="img" />
             <p>Express.js</p>
           </div>
         </div>
         <br /> <br />
         <div className="center">
           <div className="center skill-card">
-            <img src="./images/question-mark-logo.png" alt="img" />
+            <img src="/question-mark-logo.png" alt="img" />
             <p> Always ready to learn...</p>
           </div>
         </div>
